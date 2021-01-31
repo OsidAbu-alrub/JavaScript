64bf43e5cee7e783b84157aa516383454ec54b78
Osid Abu-Alrub <abualrub.osid@gmail.com>
2021-01-31 21:00:07 +0200

    popup (modal)

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
-let board = document.getElementById("game-board");
-let generateBtn = document.getElementById("gen-btn");
+const openModalBtn = document.getElementById("open-modal");
+const closeModalBtn = document.getElementById("close-modal");
+const container = document.getElementsByClassName("container")[0];
+const overlay = document.getElementById("overlay");
 
-console.log(board);
-console.log(generateBtn);
+openModalBtn.addEventListener("click", ()=>{
+    overlay.style.display = "block";
+    container.style.display = "block";
+    container.classList.toggle("dim");
+})
 
-function generateBoard() {
-    board.innerHTML="";
-    let rows = Math.random()*10;
-    let cols = Math.random()*20;
-    for (let i = 0; i < rows; i++) {
-        let tr = document.createElement("tr");
-        tr.setAttribute("class", "row");
-        for(let j = 0 ; j < cols ; j++){
-            let td = document.createElement("td");
-            td.setAttribute("class","cell");
-            tr.appendChild(td);
-        }
-        board.appendChild(tr);
-    }
-}
-
-function colorCell(board){
-    const color = document.querySelector("#color-picker").value;
-    board.target.style.backgroundColor= color; 
-}
-board.addEventListener("click", colorCell,false);
-
-generateBtn.addEventListener("click", ()=>{
-    generateBoard();
-});
-
-
-
+closeModalBtn.addEventListener("click", ()=>{
+    overlay.style.display = "none";
+    container.style.display = "none";
+    container.classList.toggle("dim");
+})
