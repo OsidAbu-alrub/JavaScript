068ebd18ebe6798e452738cb6d6e1ef4ac7bb136
Osid Abu-Alrub <abualrub.osid@gmail.com>
2021-02-02 14:33:08 +0200

    animation very cool

--- index.js
+++
@@ -1,8 +0,0 @@
-const navBar = document.querySelector(".nav-links-container");
-const navToggle = document.querySelector(".nav-toggle");
-console.log(navBar);
-console.log(navToggle);
-
-navToggle.addEventListener("click", ()=>{
-    navBar.classList.toggle("show-links");
-});
