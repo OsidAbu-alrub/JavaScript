970de014d8f24c5c8058845d5999cbdc76218a03
Osid Abu-Alrub <abualrub.osid@gmail.com>
2021-02-02 10:01:37 +0200

    nav bar

--- index.js
+++ index.js
@@ -1,41 +1,8 @@
-const rollBtn = document.querySelector(".roll-btn");
-const players = document.getElementsByClassName("player");
-const currentPlayer = document.querySelector(".player-turn");
-let playerTurn = 0;
+const navBar = document.querySelector(".nav-links-container");
+const navToggle = document.querySelector(".nav-toggle");
+console.log(navBar);
+console.log(navToggle);
 
-rollBtn.addEventListener("click", function () {
-  const val = Math.floor(Math.random() * 6) + 1;
-
-  let diceResult = players[playerTurn].querySelector(".dice-result");
-  const scoreWrapper = players[playerTurn].querySelector(".score-wrapper");
-
-  diceResult.textContent = val;
-  scoreWrapper.textContent = parseInt(scoreWrapper.textContent) + val;
-  diceResult.classList.toggle("turn");
-
-  if (parseInt(scoreWrapper.textContent) + val >= 20) {
-    console.log("test");
-    rollBtn.textContent = playerTurn + 1 + " Wins! 🎉";
-    reset();
-  } else 
-  {
-    // switch turns
-    if (playerTurn) {
-      playerTurn = 0;
-    } else {
-      playerTurn = 1;
-    }
-    diceResult = players[playerTurn].querySelector(".dice-result");
-    diceResult.classList.toggle("turn");
-
-    currentPlayer.textContent = playerTurn + 1;
-  }
+navToggle.addEventListener("click", ()=>{
+    navBar.classList.toggle("show-links");
 });
-
-function reset() {
-  playerTurn = 0;
-  let diceResult = players[playerTurn].querySelector(".dice-result");
-  currentPlayer.textContent = playerTurn + 1;
-  diceResult.classList.add("turn");
-  rollBtn.textContent = "Roll Dice 🎲";
-}
